fb8ef6cc9121ef0bc152f7008d4f1aeff2728580
Chouia Abderrahman <84145107+Chouia-Abderrahman@users.noreply.github.com>
2024-07-14 11:41:37 +0100

    Update test_model.py

--- custom_addons/module_custom/models/test_model.py
+++ custom_addons/module_custom/models/test_model.py
@@ -1,16 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-import babel.dates
 import logging
-
 from datetime import datetime, timedelta
-
 from odoo import _, models
 from odoo.exceptions import AccessDenied, UserError
 from odoo.http import request
 from odoo.tools.misc import babel_locale_parse, hmac
-
-from odoo.addons.auth_totp.models.totp import hotp, TOTP
 from odoo.odoo import fields
 
 
